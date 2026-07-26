3890a1c9d2e7b80bbec3f461f3cc9450279349c3
Barbaron86 <kirill.molodykh@mail.ru>
2026-07-26 14:27:02 +0300

    Практика: работа с логированием в нагрузочных тестах

--- tools/logger.py
+++ tools/logger.py
@@ -5,7 +5,7 @@ def get_logger(name: str) -> logging.Logger:
     logger.setLevel(logging.DEBUG)
 
 
-    if logger.hasHandlers():
+    if not logger.handlers:
         handler = logging.StreamHandler()
         handler.setLevel(logging.DEBUG)
 
@@ -14,6 +14,6 @@ def get_logger(name: str) -> logging.Logger:
 
 
         logger.addHandler(handler)
-
+    logger.propagate = False
     return logger
 
