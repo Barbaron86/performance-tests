0daa809a264f0d9d3cbc6a795de5dd2d2917876f
Barbaron86 <kirill.molodykh@mail.ru>
2026-07-26 15:25:40 +0300

    Добавляем цветное форматирование и эмодзи в логгер
    Реализован класс `ColoredFormatter` для цветового выделения различных уровней логирования в консоли с добавлением соответствующих эмодзи.

--- tools/logger.py
+++ tools/logger.py
@@ -1,19 +1,52 @@
 import logging
+import copy
+
+
+class ColoredFormatter(logging.Formatter):  # [8]
+
+    RESET = "\033[0m"
+    COLORS = {
+        logging.DEBUG: "\033[34m",  # Синий
+        logging.INFO: "\033[32m",  # Зеленый
+        logging.WARNING: "\033[33m",  # Желтый
+        logging.ERROR: "\033[31m",  # Красный
+        logging.CRITICAL: "\033[31;1m"  # Ярко-красный
+    }
+
+    EMOJIS = {
+        logging.DEBUG: "🔍 ",
+        logging.INFO: "🚀 ",
+        logging.WARNING: "🟡 ",
+        logging.ERROR: "❌ ",
+        logging.CRITICAL: "🚨 "
+    }
+
+    def format(self, record: logging.LogRecord):
+        record = copy.copy(record)
+
+        color = self.COLORS.get(record.levelno, "")
+        emoji = self.EMOJIS.get(record.levelno, "")
+
+        if color:
+            record.levelname = f"{color}{record.levelname}{self.RESET}"
+
+        record.msg = f"{emoji}{record.msg}"
+
+        return super().format(record)
+
 
 def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
-
     if not logger.handlers:
         handler = logging.StreamHandler()
         handler.setLevel(logging.DEBUG)
 
-        formater = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
-        handler.setFormatter(formater)
-
+        formatter = ColoredFormatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+        handler.setFormatter(formatter)
 
         logger.addHandler(handler)
+
     logger.propagate = False
     return logger
-
